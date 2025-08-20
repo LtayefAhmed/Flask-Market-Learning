4149264de19ded797d2b1db825c6880e23544fce
Ahmed Ltayef <ahmedltayef8@gmail.com>
2025-08-20 04:20:11 +0100

    Feat: Add completed  authentification feature

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'in
 app.config['SECRET_KEY'] = 'd3b21261d191903432e37855'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
 
 
 
